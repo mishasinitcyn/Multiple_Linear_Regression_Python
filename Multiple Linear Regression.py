7dbd281f19a8e79ecd1f2bda05b90a9c180faceb
mishasinitcyn <45673816+mishasinitcyn@users.noreply.github.com>
2021-11-08 19:08:01 -0800

    Update Multiple Linear Regression.py

--- Multiple Linear Regression.py
+++ Multiple Linear Regression.py
@@ -3,7 +3,7 @@
 #BUILT ON CODE TAUGHT IN MACHINE LEARNING COURSE BY KIRILL EREMENKO
 #MISHA SINITCYN 2021
 
-#IMPORTING LIBRARIES
+#IMPORT LIBRARIES
 import numpy as np
 import matplotlib
 import numpy as np
@@ -12,7 +12,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
-#IMPORTING DATASET
+#IMPORT DATASET
 dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
