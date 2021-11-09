ea462bd3df1acb98da507c4200a15daa7c66ba00
mishasinitcyn <45673816+mishasinitcyn@users.noreply.github.com>
2021-11-08 19:01:56 -0800

    Update Multiple Linear Regression.py

--- Multiple Linear Regression.py
+++ Multiple Linear Regression.py
@@ -1,3 +1,8 @@
+#MULTIPLE LINEAR REGRESSION USING PANDAS, MATPLOTLIB, SKLEARN, AND NUMPY
+#PROGRAM ANALYZES CSV FILE DATA, PREDICTS VALUES, AND GRAPHS THE ERROR
+#MISHA SINITCYN 2021
+#BUILT ON CODE TAUGHT IN MACHINE LEARNING COURSE BY KIRILL EREMENKO
+
 #IMPORTING LIBRARIES
 import numpy as np
 import matplotlib
@@ -6,6 +11,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+
 #IMPORTING DATASET
 dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
@@ -17,35 +23,31 @@ from sklearn.preprocessing import OneHotEncoder
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
 X = np.array(ct.fit_transform(X))
 
+#SPLIT INTO TRAINING AND TEST SET
+random__state = 10
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = random__state)
+error_array = [0,0,0,0,0,0,0,0,0,0]
 
-for j in range(0,10):
-    #SPLIT INTO TRAINING AND TEST SET
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = j)
+#TRAIN ON TRAIN SET
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
 
-    #TRAIN ON TRAIN SET
-    regressor = LinearRegression()
-    regressor.fit(X_train, y_train)
+#PREDICTING THE TEST SET RESULTS
+y_pred = regressor.predict(X_test)
 
-    #PREDICTING THE TEST SET RESULTS
-    y_pred = regressor.predict(X_test)
+#PRINT PREDICTED VALUES, ACTUAL VALUES, AND MARGIN OF ERROR
+for i in range(0,10):
+    print( round(y_pred[i],2), round(y_test[i],2), round(abs((y_test[i] - y_pred[i])/y_test[i])*100,2 ), "%" )
+    error_array[i-1] += round( abs(((y_test[i] - y_pred[i])/y_test[i]) * 100), 2)
 
-    a_error = 0
-    error_array = [0,0,0,0,0,0,0,0,0,0]
+#MARGIN ERROR PER PREDICTED VALUE, AS SHOWN IN GRAPH
+print(error_array)
+x_axis = [0,1,2,3,4,5,6,7,8,9]
 
-    for i in range(0,10):
-        print( round(y_pred[i],2), round(y_test[i],2), round((y_pred[i]/y_test[i])*100,2 ) )
-        a_error += (y_pred[i]/y_test[i])**2
-        #error_array[i-1] += round((y_pred[i]/y_test[i])**2 * 100, 2)
-        error_array[i-1] += round( abs((y_pred[i]/y_test[i]) * 100 - 100), 2)
-        
-    print(error_array)
-    x_axis = [0,1,2,3,4,5,6,7,8,9]
-
-    plt.figure(j)
-    plt.plot(x_axis, error_array,  color='xkcd:salmon', label='original')
-    plt.legend()
-    plt.show()
-    plt.close(j)
-
-    print()
-    print(a_error)
+#GRAPH THE ERROR
+plt.figure(0)
+plt.plot(x_axis, error_array,  color='xkcd:salmon', label='original')
+plt.legend()
+plt.show()
+plt.close(0)
+print()
